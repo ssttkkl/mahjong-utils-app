570eba1382c3dc789d89a94d8f145282a9342a60
ssttkkl <huang.wen.long@hotmail.com>
2024-01-06 02:10:53 +0800

    压缩贴图尺寸

--- composeApp/src/iosMain/kotlin/io/ssttkkl/mahjongutils/app/utils/ImageUtils.ios.kt
+++ composeApp/src/iosMain/kotlin/io/ssttkkl/mahjongutils/app/utils/ImageUtils.ios.kt
@@ -1,6 +1,7 @@
 package io.ssttkkl.mahjongutils.app.utils
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.toComposeImageBitmap
 import dev.icerock.moko.resources.ImageResource
@@ -48,49 +49,51 @@ actual fun ImageResource.toImageBitmap(): ImageBitmap {
 @OptIn(ExperimentalForeignApi::class)
 @Composable
 actual fun ImageResource.toLieDownImageBitmap(): ImageBitmap {
-    // https://gist.github.com/paolonl/6231410
-    val imgRef = this.toUIImage()?.CGImage()
-    val angleInRadians = (90) * (M_PI / 180)
-    val width = CGImageGetWidth(imgRef).toDouble()
-    val height = CGImageGetHeight(imgRef).toDouble()
+    return remember(this) {
+        // https://gist.github.com/paolonl/6231410
+        val imgRef = this.toUIImage()?.CGImage()
+        val angleInRadians = (90) * (M_PI / 180)
+        val width = CGImageGetWidth(imgRef).toDouble()
+        val height = CGImageGetHeight(imgRef).toDouble()
 
-    val imgRect = CGRectMake(
-        0.0,
-        0.0,
-        width,
-        height
-    )
-    val transform = CGAffineTransformMakeRotation(angleInRadians)
-    val rotatedRect = CGRectApplyAffineTransform(imgRect, transform)
+        val imgRect = CGRectMake(
+            0.0,
+            0.0,
+            width,
+            height
+        )
+        val transform = CGAffineTransformMakeRotation(angleInRadians)
+        val rotatedRect = CGRectApplyAffineTransform(imgRect, transform)
 
-    val colorSpace = CGColorSpaceCreateDeviceRGB()
-    val bmContext = CGBitmapContextCreate(
-        null,
-        rotatedRect.useContents { size.width }.toULong(),
-        rotatedRect.useContents { size.height }.toULong(),
-        CGImageGetBitsPerComponent(imgRef),
-        0u,
-        colorSpace,
-        CGImageAlphaInfo.kCGImageAlphaPremultipliedFirst.value
-    )
+        val colorSpace = CGColorSpaceCreateDeviceRGB()
+        val bmContext = CGBitmapContextCreate(
+            null,
+            rotatedRect.useContents { size.width }.toULong(),
+            rotatedRect.useContents { size.height }.toULong(),
+            CGImageGetBitsPerComponent(imgRef),
+            0u,
+            colorSpace,
+            CGImageAlphaInfo.kCGImageAlphaPremultipliedFirst.value
+        )
 
-    CGContextSetAllowsAntialiasing(bmContext, false)
-    CGContextSetInterpolationQuality(bmContext, kCGInterpolationNone);
-    CGColorSpaceRelease(colorSpace);
-    CGContextTranslateCTM(
-        bmContext,
-        rotatedRect.useContents { size.width / 2 }.toDouble(),
-        rotatedRect.useContents { size.height / 2 }.toDouble()
-    )
-    CGContextRotateCTM(bmContext, angleInRadians);
-    CGContextDrawImage(
-        bmContext, CGRectMake(-width / 2, -height / 2, width, height),
-        imgRef
-    )
+        CGContextSetAllowsAntialiasing(bmContext, false)
+        CGContextSetInterpolationQuality(bmContext, kCGInterpolationNone);
+        CGColorSpaceRelease(colorSpace);
+        CGContextTranslateCTM(
+            bmContext,
+            rotatedRect.useContents { size.width / 2 }.toDouble(),
+            rotatedRect.useContents { size.height / 2 }.toDouble()
+        )
+        CGContextRotateCTM(bmContext, angleInRadians);
+        CGContextDrawImage(
+            bmContext, CGRectMake(-width / 2, -height / 2, width, height),
+            imgRef
+        )
 
-    val rotatedImage = CGBitmapContextCreateImage(bmContext);
-    CFRelease(bmContext);
+        val rotatedImage = CGBitmapContextCreateImage(bmContext);
+        CFRelease(bmContext);
 
-    return rotatedImage?.toSkiaImage()?.toComposeImageBitmap()
-        ?: error("fail to convert $this to ImageBitmap")
+        rotatedImage?.toSkiaImage()?.toComposeImageBitmap()
+            ?: error("fail to convert $this to ImageBitmap")
+    }
 }
