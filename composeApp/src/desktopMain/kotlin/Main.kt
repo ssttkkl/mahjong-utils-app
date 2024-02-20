74438fdc2af12dd5e4c5219fea95d61cb2ce52d8
ssttkkl <huang.wen.long@hotmail.com>
2024-02-21 01:10:23 +0800

    桌面版添加标题

--- composeApp/src/desktopMain/kotlin/Main.kt
+++ composeApp/src/desktopMain/kotlin/Main.kt
@@ -2,10 +2,16 @@ import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import dev.icerock.moko.resources.desc.Resource
+import dev.icerock.moko.resources.desc.StringDesc
 import io.ssttkkl.mahjongutils.app.App
+import io.ssttkkl.mahjongutils.app.MR
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication, title = "KotlinProject") {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = StringDesc.Resource(MR.strings.app_name).localized()
+    ) {
         App()
     }
 }
