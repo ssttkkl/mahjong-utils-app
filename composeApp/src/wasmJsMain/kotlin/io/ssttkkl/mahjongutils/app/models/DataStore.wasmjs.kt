e766d7a2cc9d551ec41b53b8e93791d5c5b9c733
ssttkkl <huang.wen.long@hotmail.com>
2024-02-23 21:33:44 +0800

    实现wasmJs的数据存储

--- composeApp/src/wasmJsMain/kotlin/io/ssttkkl/mahjongutils/app/models/DataStore.wasmjs.kt
+++ composeApp/src/wasmJsMain/kotlin/io/ssttkkl/mahjongutils/app/models/DataStore.wasmjs.kt
@@ -1,9 +1,12 @@
 package io.ssttkkl.mahjongutils.app.models
 
+import kotlinx.browser.localStorage
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.update
 import kotlinx.serialization.KSerializer
+import kotlinx.serialization.json.Json
+import org.w3c.dom.get
+import org.w3c.dom.set
 
 actual fun <T> createDataStore(
     identifer: String,
@@ -11,12 +14,24 @@ actual fun <T> createDataStore(
     defaultValue: T,
     serializer: KSerializer<T>
 ): DataStore<T> = object : DataStore<T> {
-    private val _data = MutableStateFlow(defaultValue)
+    private val localStorageKey = "${groupIdenifier}/${identifer}"
+
+    private val _data: MutableStateFlow<T>
+
+    init {
+        val initialValue = localStorage[localStorageKey]?.let {
+            Json.decodeFromString(serializer, it)
+        } ?: defaultValue
+
+        _data = MutableStateFlow(initialValue)
+    }
     override val data: Flow<T>
         get() = _data
 
     override suspend fun updateData(transform: suspend (t: T) -> T): T {
-        _data.update { transform(it) }
-        return _data.value
+        val updatedValue = transform(_data.value)
+        localStorage[localStorageKey] = Json.encodeToString(serializer, updatedValue)
+        _data.value = updatedValue
+        return updatedValue
     }
 }
