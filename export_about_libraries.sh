525b7920e13d08765e9abd778099340de895ae6c
ssttkkl <huang.wen.long@hotmail.com>
2024-02-23 11:52:03 +0800

    更新export_about_libraries.sh

--- export_about_libraries.sh
+++ export_about_libraries.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-./gradlew composeApp:exportLibraryDefinitions -PaboutLibraries.exportPath=src/commonMain/resources/MR/files/ -PaboutLibraries.exportVariant=release
+./gradlew composeApp:exportLibraryDefinitions -PaboutLibraries.exportPath=src/commonMain/composeResources/files/ -PaboutLibraries.exportVariant=release
