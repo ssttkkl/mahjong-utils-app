abbf2f297371943490c9e1f418be94f45b8eacc7
ssttkkl <huang.wen.long@hotmail.com>
2024-08-25 21:08:16 +0800

    移除无用仓库

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,11 +3,9 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
     repositories {
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         google()
         gradlePluginPortal()
         mavenCentral()
-        maven("https://s01.oss.sonatype.org/service/local/repositories/releases/content")
         mavenLocal()
     }
 }
@@ -16,8 +14,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-        maven("https://s01.oss.sonatype.org/service/local/repositories/releases/content")
         mavenLocal()
     }
 }
