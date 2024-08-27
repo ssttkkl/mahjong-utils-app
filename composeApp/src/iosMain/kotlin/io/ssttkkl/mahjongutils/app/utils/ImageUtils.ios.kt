51d2c8070280b24ae62f3613567b03cf90c950f6
huangwenlong06 <huangwenlong06@meituan.com>
2024-08-27 11:43:00 +0800

    修复ios编译

--- composeApp/src/iosMain/kotlin/io/ssttkkl/mahjongutils/app/utils/ImageUtils.ios.kt
+++ composeApp/src/iosMain/kotlin/io/ssttkkl/mahjongutils/app/utils/ImageUtils.ios.kt
@@ -1,88 +1,34 @@
+@file:Suppress("INVISIBLE_REFERENCE", "INVISIBLE_MEMBER")
+
 package io.ssttkkl.mahjongutils.app.utils
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.toComposeImageBitmap
 import org.jetbrains.compose.resources.DrawableResource
-import kotlinx.cinterop.ExperimentalForeignApi
-import kotlinx.cinterop.useContents
-import platform.CoreFoundation.CFRelease
-import platform.CoreGraphics.CGAffineTransformMakeRotation
-import platform.CoreGraphics.CGBitmapContextCreate
-import platform.CoreGraphics.CGBitmapContextCreateImage
-import platform.CoreGraphics.CGColorSpaceCreateDeviceRGB
-import platform.CoreGraphics.CGColorSpaceRelease
-import platform.CoreGraphics.CGContextDrawImage
-import platform.CoreGraphics.CGContextRotateCTM
-import platform.CoreGraphics.CGContextSetAllowsAntialiasing
-import platform.CoreGraphics.CGContextSetInterpolationQuality
-import platform.CoreGraphics.CGContextTranslateCTM
-import platform.CoreGraphics.CGImageAlphaInfo
-import platform.CoreGraphics.CGImageGetBitsPerComponent
-import platform.CoreGraphics.CGImageGetHeight
-import platform.CoreGraphics.CGImageGetWidth
-import platform.CoreGraphics.CGRectApplyAffineTransform
-import platform.CoreGraphics.CGRectMake
-import platform.CoreGraphics.kCGInterpolationNone
+import org.jetbrains.compose.resources.InternalResourceApi
+import org.jetbrains.compose.resources.LocalComposeEnvironment
+import org.jetbrains.compose.resources.LocalResourceReader
+import org.jetbrains.compose.resources.getResourceItemByEnvironment
+import platform.Foundation.NSBundle
+import platform.Foundation.NSFileManager
 import platform.UIKit.UIImage
-import platform.posix.M_PI
 
 
+@OptIn(InternalResourceApi::class)
 @Composable
 fun DrawableResource.toUIImage(): UIImage {
-    TODO()
-}
+    val resource = this
+    val environment = LocalComposeEnvironment.current.rememberEnvironment()
+    val filePath =
+        remember(resource, environment) { resource.getResourceItemByEnvironment(environment).path }
+    if (filePath.endsWith(".xml", true) || filePath.endsWith(".svg", true)) {
+        error("cannot convert vector")
+    }
 
-@OptIn(ExperimentalForeignApi::class)
-@Composable
-actual fun DrawableResource.toLieDownImageBitmap(): ImageBitmap {
-    val uiImage = this.toUIImage()
-    return remember(this) {
-        // https://gist.github.com/paolonl/6231410
-        val imgRef = uiImage.CGImage()
-        val angleInRadians = (90) * (M_PI / 180)
-        val width = CGImageGetWidth(imgRef).toDouble()
-        val height = CGImageGetHeight(imgRef).toDouble()
-
-        val imgRect = CGRectMake(
-            0.0,
-            0.0,
-            width,
-            height
-        )
-        val transform = CGAffineTransformMakeRotation(angleInRadians)
-        val rotatedRect = CGRectApplyAffineTransform(imgRect, transform)
-
-        val colorSpace = CGColorSpaceCreateDeviceRGB()
-        val bmContext = CGBitmapContextCreate(
-            null,
-            rotatedRect.useContents { size.width }.toULong(),
-            rotatedRect.useContents { size.height }.toULong(),
-            CGImageGetBitsPerComponent(imgRef),
-            0u,
-            colorSpace,
-            CGImageAlphaInfo.kCGImageAlphaPremultipliedFirst.value
-        )
-
-        CGContextSetAllowsAntialiasing(bmContext, false)
-        CGContextSetInterpolationQuality(bmContext, kCGInterpolationNone);
-        CGColorSpaceRelease(colorSpace);
-        CGContextTranslateCTM(
-            bmContext,
-            rotatedRect.useContents { size.width / 2 }.toDouble(),
-            rotatedRect.useContents { size.height / 2 }.toDouble()
-        )
-        CGContextRotateCTM(bmContext, angleInRadians);
-        CGContextDrawImage(
-            bmContext, CGRectMake(-width / 2, -height / 2, width, height),
-            imgRef
-        )
-
-        val rotatedImage = CGBitmapContextCreateImage(bmContext);
-        CFRelease(bmContext);
-
-        rotatedImage?.toSkiaImage()?.toComposeImageBitmap()
-            ?: error("fail to convert $this to ImageBitmap")
+    val resourceReader = LocalResourceReader.current
+    return remember(filePath, resourceReader) {
+        val data = NSFileManager.defaultManager()
+            .contentsAtPath(NSBundle.mainBundle.resourcePath + "/compose-resources/" + filePath)
+        data?.let { UIImage.imageWithData(it) } ?: error("failed to load image")
     }
 }
