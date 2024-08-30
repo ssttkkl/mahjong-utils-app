6319d4e51aeff62181861d969e2a9502ca68b3bc
ssttkkl <huang.wen.long@hotmail.com>
2024-08-30 15:08:53 +0800

    cleanup

--- composeApp/src/androidMain/kotlin/io/ssttkkl/mahjongutils/app/utils/image/ImageUtils.android.kt
+++ composeApp/src/androidMain/kotlin/io/ssttkkl/mahjongutils/app/utils/image/ImageUtils.android.kt
@@ -1,13 +1,20 @@
 package io.ssttkkl.mahjongutils.app.utils.image
 
+import android.content.ContentValues
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Paint
+import android.net.Uri
+import android.os.Environment
+import android.provider.MediaStore
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asAndroidBitmap
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.toArgb
+import io.ssttkkl.mahjongutils.app.MyApp
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 actual suspend fun platformWithBackground(
     imageBitmap: ImageBitmap,
@@ -34,4 +41,37 @@ actual suspend fun platformWithBackground(
     canvas.drawBitmap(imageBitmap.asAndroidBitmap(), 50f, 50f, null) // 偏移位置50, 50
 
     return newBitmap.asImageBitmap()
+}
+
+actual object ImageUtils : CommonImageUtils() {
+    actual suspend fun save(imageBitmap: ImageBitmap, title: String): Boolean {
+        return withContext(Dispatchers.IO) {
+            val values = ContentValues().apply {
+                put(MediaStore.Images.Media.DISPLAY_NAME, title)
+                put(MediaStore.Images.Media.MIME_TYPE, "image/png")
+                put(MediaStore.Images.Media.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                put(MediaStore.Images.Media.IS_PENDING, 1)
+            }
+
+            val uri: Uri = MyApp.current.contentResolver.insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                values
+            ) ?: return@withContext false
+
+            uri.let {
+                try {
+                    MyApp.current.contentResolver.openOutputStream(it)?.use {
+                        imageBitmap.asAndroidBitmap().compress(Bitmap.CompressFormat.PNG, 100, it)
+                    } ?: return@let false
+                    values.clear()
+                    values.put(MediaStore.Images.Media.IS_PENDING, 0)
+                    MyApp.current.contentResolver.update(uri, values, null, null)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    return@let false
+                }
+            }
+            return@withContext true
+        }
+    }
 }
