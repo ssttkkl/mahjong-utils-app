606947bab4fc7314f719ce766059564b95ecf249
ssttkkl <huang.wen.long@hotmail.com>
2025-02-28 14:19:02 +0800

    升级基础依赖

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,18 +21,18 @@ include(":composeApp")
 
 val envPropFile = file("env.properties")
 
-val mahjongUtilsLibPath = file("external/mahjong-utils")
-if (mahjongUtilsLibPath.resolve("build.gradle.kts").exists()) {
-    if (envPropFile.exists()) {
-        envPropFile.copyTo(mahjongUtilsLibPath.resolve("env.properties"), overwrite = true)
-    }
-    includeBuild(mahjongUtilsLibPath.path) {
-        name = "mahjong-utils-lib"
-        dependencySubstitution {
-            substitute(module("io.github.ssttkkl:mahjong-utils")).using(project(":mahjong-utils"))
-        }
-    }
-}
+//val mahjongUtilsLibPath = file("external/mahjong-utils")
+//if (mahjongUtilsLibPath.resolve("build.gradle.kts").exists()) {
+//    if (envPropFile.exists()) {
+//        envPropFile.copyTo(mahjongUtilsLibPath.resolve("env.properties"), overwrite = true)
+//    }
+//    includeBuild(mahjongUtilsLibPath.path) {
+//        name = "mahjong-utils-lib"
+//        dependencySubstitution {
+//            substitute(module("io.github.ssttkkl:mahjong-utils")).using(project(":mahjong-utils"))
+//        }
+//    }
+//}
 
 val capturableLibPath = file("external/Capturable")
 if (envPropFile.exists()) {
