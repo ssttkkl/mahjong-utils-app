c014e24c132e8eee9c69ca3560f62d5592f3e4a4
ssttkkl <huang.wen.long@hotmail.com>
2025-03-29 00:56:27 +0800

    code cleanup

--- composeApp/src/commonMain/kotlin/io/ssttkkl/mahjongutils/app/components/tileime/TileIme.kt
+++ composeApp/src/commonMain/kotlin/io/ssttkkl/mahjongutils/app/components/tileime/TileIme.kt
@@ -11,18 +11,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material3.Card
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
@@ -32,16 +29,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.unit.dp
 import io.ssttkkl.mahjongutils.app.components.backhandler.BackHandler
 import io.ssttkkl.mahjongutils.app.components.clickableButNotFocusable
 import io.ssttkkl.mahjongutils.app.components.tile.TileImage
-import io.ssttkkl.mahjongutils.app.components.tile.Tiles
 import io.ssttkkl.mahjongutils.app.components.tileime.TileImeHostState.ImeAction
-import io.ssttkkl.mahjongutils.app.utils.Spacing
 import mahjongutils.composeapp.generated.resources.Res
 import mahjongutils.composeapp.generated.resources.icon_content_copy
 import mahjongutils.composeapp.generated.resources.icon_content_paste
@@ -174,7 +167,10 @@ private fun TilePopMenu(state: TileImeHostState, modifier: Modifier) {
                 text = {
                     Row(Modifier.padding(vertical = 8.dp)) {
                         Icon(painterResource(Res.drawable.icon_content_copy), "")
-                        Text(stringResource(Res.string.label_copy), Modifier.padding(horizontal = 8.dp))
+                        Text(
+                            stringResource(Res.string.label_copy),
+                            Modifier.padding(horizontal = 8.dp)
+                        )
                     }
                 },
                 onClick = {
@@ -216,7 +212,10 @@ private fun TilePopMenu(state: TileImeHostState, modifier: Modifier) {
                 text = {
                     Row(Modifier.padding(vertical = 8.dp)) {
                         Icon(Icons.Default.Clear, "")
-                        Text(stringResource(Res.string.label_clear), Modifier.padding(horizontal = 8.dp))
+                        Text(
+                            stringResource(Res.string.label_clear),
+                            Modifier.padding(horizontal = 8.dp)
+                        )
                     }
                 },
                 onClick = {
