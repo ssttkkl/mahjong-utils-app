edd870bb2601dc5f62b5d1e2a7d2b045d26099c5
ssttkkl <huang.wen.long@hotmail.com>
2025-03-29 19:27:36 +0800

    Update Build Actions

---
+++ build-on-vercel.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+curl -L -o /tmp/openjdk-17.tar.gz https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.14%2B7/OpenJDK17U-jdk_x64_linux_hotspot_17.0.14_7.tar.gz
+tar -xzf /tmp/openjdk-17.tar.gz -C /tmp/
+
+export JAVA_HOME=/tmp/jdk-17.0.14+7
+export PATH=$PATH:$JAVA_HOME/bin
+
+./gradlew composeApp:wasmJsBrowserDistribution --no-daemon
