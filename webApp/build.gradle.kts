e484816a7ad1f27797de02c99366b1447024463f
ssttkkl <huang.wen.long@hotmail.com>
2025-08-01 01:40:49 +0800

    移动模型文件

--- webApp/build.gradle.kts
+++ webApp/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
 }
 
 kotlin.sourceSets.wasmJsMain {
+    // 之前给fdroid的配置里，模型就放在这里，不好改了
+    resources.srcDir(project(":mahjong-detector").file("src/wasmJsMain/resources"))
     dependencies {
         api(project(":shared"))
     }
